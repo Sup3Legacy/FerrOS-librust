dd552f4db844de39ddfcc36594c10f11b7d10893
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-10 22:16:08 +0200

    update syscall list

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -46,11 +46,21 @@ pub extern "C" fn syscall(nb: u64, arg0: u64, arg1: u64, arg2: u64, arg3: u64, a
 }
 
 pub unsafe fn read(file_descriptor: usize, buffer: *mut usize, count: usize) -> usize {
-    todo!()
+    let response;
+    asm!(
+        "int 80h",
+        in("rax") 0, in("rsi") buffer, in("rdx") count, lateout("rax") response
+    )
+    response
 }
 
 pub unsafe fn write(file_descriptor: usize, buffer: *mut usize, count: usize) -> usize {
-    todo!()
+    let response;
+    asm!(
+        "int 80h",
+        in("rax") 1, in("rsi") buffer, in("rdx") count, lateout("rax") response
+    )
+    response
 }
 
 /// Maybe we can pass a whole String for the path
@@ -62,8 +72,88 @@ pub unsafe fn close(file_descriptor: usize) -> usize {
     syscall(3, file_descriptor as u64, 0, 0, 0, 0) as usize
 }
 
+pub unsafe fn dup2(fd1: usize, fd2: usize) -> usize {
+    syscall(4, fd1, fd2, 0, 0, 0) as usize
+}
+
+pub unsafe fn fork() -> usize {
+    syscall(5, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn exec(name: String) -> usize {
+    syscall(6, name.as_ptr(), 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn exit(code: usize) -> usize {
+    syscall(7, code, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn sleep() {
+    syscall(8, 0, 0, 0, 0, 0);
+}
+
+pub unsafe fn shutdown(code: usize) -> usize {
+    syscall(9, code, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn get_puid() -> usize {
+    syscall(10, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn set_screen_size(height: usize, width: usize) -> usize {
+    syscall(11, height, width, 0, 0, 0) as usize
+}
+
+pub unsafe fn set_screen_pos(top: usize, left: usize) -> usize {
+    syscall(12, top, left, 0, 0, 0) as usize
+}
+
+pub unsafe fn getcwd() -> usize {
+    syscall(13, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn chdir() -> usize {
+    syscall(14, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn mkdir() -> usize {
+    syscall(15, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn rmdir() -> usize {
+    syscall(16, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn get_layer() -> usize {
+    syscall(17, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn set_layer() -> usize {
+    syscall(18, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn set_focus() -> usize {
+    syscall(19, 0, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn debug(v1: usize, v2: usize) {
+    syscall(20, v1, v2, 0, 0, 0);
+}
+
 pub unsafe fn memrequest(number: usize) -> usize {
     syscall(21, number as u64, 0, 0, 0, 0) as usize
+}
+
+pub unsafe fn listen() -> (usize, usize) {
+    let res1;
+    let res2;
+    asm!(
+        "int 80h",
+        in(rax) 22,
+        lateout(rax) res1,
+        lateout(rdi) res2,
+    )
+    (res1, res2)
 }
 
 #[repr(C)]
