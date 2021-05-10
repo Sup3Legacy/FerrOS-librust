e7383bdbe28b445b22b22a8378256287bf4817da
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-05-10 22:30:04 +0200

    finished syscall

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -1,5 +1,7 @@
 use alloc::string::String;
 
+use x86_64::VirtAddr;
+
 // callee-saved : rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
 // syscall : rax, rdi, rsi, rdx, r10, r8, r9
 // param : rdi, rsi, rdx, rcx, r8, r9
@@ -28,19 +30,14 @@ pub unsafe extern "C" fn syscall_old(
 }
 
 #[inline(never)]
-pub extern "C" fn syscall(nb: u64, arg0: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64) -> usize {
+pub extern "C" fn syscall(nb: usize, arg0: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
     let res;
     unsafe {
         asm!(
-            "mov rax, {}", 
-            "mov rdi, {}",
-            "mov rsi, {}",
-            "mov rdx, {}",
-            "mov r10, {}",
-            "mov r8, {}",
             "int 80h",
-            "mov {}, rax", 
-            in(reg) nb, in(reg) arg0, in(reg) arg1, in(reg) arg2, in(reg) arg3, in(reg) arg4, out(reg) res)
+            in("rax") nb, in("rdi") arg0, in("rsi") arg1, in("rdx") arg2, in("r10") arg3, in("r8") arg4,
+            lateout("rax") res
+        )
     };
     res
 }
@@ -50,7 +47,7 @@ pub unsafe fn read(file_descriptor: usize, buffer: *mut usize, count: usize) -> 
     asm!(
         "int 80h",
         in("rax") 0, in("rsi") buffer, in("rdx") count, lateout("rax") response
-    )
+    );
     response
 }
 
@@ -59,33 +56,33 @@ pub unsafe fn write(file_descriptor: usize, buffer: *mut usize, count: usize) ->
     asm!(
         "int 80h",
         in("rax") 1, in("rsi") buffer, in("rdx") count, lateout("rax") response
-    )
+    );
     response
 }
 
 /// Maybe we can pass a whole String for the path
 pub unsafe fn open(path: *mut usize, length: usize, flags: u64) -> usize {
-    syscall(2, path as u64, length as u64, flags, 0, 0) as usize
+    syscall(2, VirtAddr::from_ptr(path).as_u64() as usize, length, flags as usize, 0, 0)
 }
 
 pub unsafe fn close(file_descriptor: usize) -> usize {
-    syscall(3, file_descriptor as u64, 0, 0, 0, 0) as usize
+    syscall(3, file_descriptor, 0, 0, 0, 0)
 }
 
 pub unsafe fn dup2(fd1: usize, fd2: usize) -> usize {
-    syscall(4, fd1, fd2, 0, 0, 0) as usize
+    syscall(4, fd1, fd2, 0, 0, 0)
 }
 
 pub unsafe fn fork() -> usize {
-    syscall(5, 0, 0, 0, 0, 0) as usize
+    syscall(5, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn exec(name: String) -> usize {
-    syscall(6, name.as_ptr(), 0, 0, 0, 0) as usize
+    syscall(6, VirtAddr::from_ptr(name.as_ptr()).as_u64() as usize, 0, 0, 0, 0)
 }
 
 pub unsafe fn exit(code: usize) -> usize {
-    syscall(7, code as u64, 0, 0, 0, 0) as usize
+    syscall(7, code, 0, 0, 0, 0)
 }
 
 pub unsafe fn sleep() {
@@ -93,39 +90,39 @@ pub unsafe fn sleep() {
 }
 
 pub unsafe fn shutdown(code: usize) -> usize {
-    syscall(9, code as u64, 0, 0, 0, 0) as usize
+    syscall(9, code, 0, 0, 0, 0)
 }
 
 pub unsafe fn get_puid() -> usize {
-    syscall(10, 0, 0, 0, 0, 0) as usize
+    syscall(10, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn set_screen_size(height: usize, width: usize) -> usize {
-    syscall(11, height as u64, width as u64, 0, 0, 0) as usize
+    syscall(11, height, width, 0, 0, 0)
 }
 
 pub unsafe fn set_screen_pos(top: usize, left: usize) -> usize {
-    syscall(12, top as u64, left as u64, 0, 0, 0) as usize
+    syscall(12, top, left, 0, 0, 0)
 }
 
 pub unsafe fn getcwd() -> usize {
-    syscall(13, 0, 0, 0, 0, 0) as usize
+    syscall(13, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn chdir() -> usize {
-    syscall(14, 0, 0, 0, 0, 0) as usize
+    syscall(14, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn mkdir() -> usize {
-    syscall(15, 0, 0, 0, 0, 0) as usize
+    syscall(15, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn rmdir() -> usize {
-    syscall(16, 0, 0, 0, 0, 0) as usize
+    syscall(16, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn get_layer() -> usize {
-    syscall(17, 0, 0, 0, 0, 0) as usize
+    syscall(17, 0, 0, 0, 0, 0)
 }
 
 pub unsafe fn set_layer() -> usize {
@@ -137,11 +134,11 @@ pub unsafe fn set_focus() -> usize {
 }
 
 pub unsafe fn debug(v1: usize, v2: usize) {
-    syscall(20, v1 as u64, v2 as u64, 0, 0, 0);
+    syscall(20, v1, v2, 0, 0, 0);
 }
 
 pub unsafe fn memrequest(number: usize) -> usize {
-    syscall(21, number as u64, 0, 0, 0, 0) as usize
+    syscall(21, number, 0, 0, 0, 0)
 }
 
 pub unsafe fn listen() -> (usize, usize) {
@@ -149,9 +146,9 @@ pub unsafe fn listen() -> (usize, usize) {
     let res2;
     asm!(
         "int 80h",
-        in(rax) 22,
-        lateout(rax) res1,
-        lateout(rdi) res2,
+        in("rax") 22,
+        lateout("rax") res1,
+        lateout("rdi") res2,
     );
     (res1, res2)
 }
